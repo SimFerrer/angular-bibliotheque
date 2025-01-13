7961d03ec7b8b686ac3af3b627035cc9eb77d720
sferrer <simon.ferrer@apside.com>
2025-01-13 14:17:26 +0100

    header / footer / home

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { JwtModule } from '@auth0/angular-jwt';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+export function tokenGetter() {
+  return localStorage.getItem("access_token");
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(
+      JwtModule.forRoot({
+          config: {
+              tokenGetter: tokenGetter,
+              allowedDomains: ['localhost:8000','localhost:8000/api'],
+              disallowedRoutes: ['localhost:8000/api/login_check','localhost:8000/api/login_check'],
+          },
+      }),
+  ),
+  provideHttpClient(withInterceptorsFromDi())
+  ]
 };
